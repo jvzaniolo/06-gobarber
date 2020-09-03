6ab28a431dcb709901bf6078d2c04173e0871e5b
João Vitor <jv.zaniolo@gmail.com>
2020-09-02 22:24:18 -0300

    initialized: signUpPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
-import SignIn from './pages/SignIn';
+// import LogIn from './pages/LogIn';
+import SignUp from './pages/SignUp';
 
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => (
   <>
-    <SignIn />
+    <SignUp />
     <GlobalStyle />
   </>
 );
