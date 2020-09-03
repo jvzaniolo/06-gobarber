5bcdd47402be5fa6ac7d8b03d413c218022d804f
João Vitor <jv.zaniolo@gmail.com>
2020-09-03 19:02:59 -0300

    add: unform para tratar formularios

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,23 +1,38 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
+interface ContainerProps {
+  isFocused: boolean;
+  hasContent: boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
   color: #666360;
   background: #232129;
   border-radius: 10px;
   border: 2px solid #232129;
-  padding: 16px;
   width: 100%;
   display: flex;
+  padding: 0 16px;
   align-items: center;
 
   & + div {
     margin-top: 8px;
   }
 
+  ${(props) => props.isFocused && css`
+    color: #FF9000;
+    border-color: #FF9000;
+  `}
+
+  ${(props) => props.hasContent && css`
+    color: #FF9000;
+  `}
+
   input {
     flex: 1;
     border: 0;
     color: #F4EDE8;
+    padding: 16px 0;
     background: transparent;
 
     &::placeholder {
@@ -26,6 +41,6 @@ export const Container = styled.div`
   }
 
   svg {
-    margin-right: 16px;
+    margin-right: 12px;
   }
 `;
